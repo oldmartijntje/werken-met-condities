1626e1abc915aba58cc8e7ab8131209d450bf679
Martijn Van Houwelingen <oldmartijntje@gmail.com>
2021-09-14 11:54:28 +0200

    verification

--- githubUpdater.bat
+++ githubUpdater.bat
@@ -1,9 +1,9 @@
 @echo off
 color a
-set /p anwser=[message]
-git commit -a -m %anwser%
-git commit -a -m %anwser%
+set /p allRightsReservedToOldMartinG=[message]
+git commit -a -m %allRightsReservedToOldMartinG%
+git commit -a -m %allRightsReservedToOldMartinG%
 git add --all
-git commit -a -m %anwser%
+git commit -a -m %allRightsReservedToOldMartinG%
 git push
 set /p anwserr=[done]
