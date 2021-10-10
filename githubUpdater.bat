7266de75b02a988d3c222d75ff7e055402af3fc0
oldmartijntje <oldmartijntje@gmail.com>
2021-10-10 16:48:23 +0200

    default commit message

--- githubUpdater.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color a
-set /p allRightsReservedToOldMartinG=[message]
-git commit -a -m %allRightsReservedToOldMartinG%
-git commit -a -m %allRightsReservedToOldMartinG%
-git add --all
-git commit -a -m %allRightsReservedToOldMartinG%
-git push
-set /p anwserr=[done]
